063281e61c8ec5fdda7640b135037a5b758a25d8
Gerome Heath <gerome247@gmail.com>
2024-12-05 15:08:34 +0000

    First commit - setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Navigate, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Users />} index/>
+      <Route path="/users" element={<Users />} />
+      <Route path="/places/new" element={<NewPlace />} />
+      <Route path="/" element={<Navigate replace to="/" />} />
+    </Routes>
+  </Router>;
 }
 
 export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NewPlace() {
+  return (
+    <h2>New Place Works!</h2>
+  );
+}
